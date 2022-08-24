4f93fbfe6b632009db4e224db0f493ba1ef98ee4
Willi Rath <wrath@geomar.de>
2022-08-24 14:53:44 +0200

    Add (untested) time interpolation

--- pm1970_xarray/slab_model.py
+++ pm1970_xarray/slab_model.py
@@ -27,3 +27,46 @@ def coriolis_parameter_f(latitude):
         pass
 
     return f
+
+
+def resample_data(dobj, dt_seconds=3600):
+    """Resample data.
+
+    Will use linear interpolation.
+
+    Parameters
+    ----------
+    dobj: xarray.DataArray or xarray.Dataset
+        Has a dimensional coordinate called "time".
+    dt_seconds: int
+        Target time step in seconds.  Defaults to 3600.
+
+    Returns
+    -------
+    xarray.DataArray or xarray.Dataset
+        Same as dobj with resampled time axis.
+
+    """
+    original_time_axis = dobj.coords["time"].copy()
+    dt_timedelta = np.timedelta64(int(dt_seconds), "s")
+
+    # transform data time axis to numbers
+    dobj.coords["time"] = original_time_axis / dt_timedelta
+
+    # construct target time axis
+    new_time_axis = xr.DataArray(
+        np.arange(
+            original_time_axis.isel(time=0).data,
+            original_time_axis.isel(time=-1).data,
+            dt_timedelta,
+        ),
+        dims=("time",),
+        name="time",
+    )
+    new_time_axis_numeric = new_time_axis / dt_timedelta
+
+    # interpolate
+    dobj_interp = dobj.interp_like(new_time_axis_numeric)
+    dobj_interp.coords["time"] = new_time_axis
+
+    return dobj_interp
